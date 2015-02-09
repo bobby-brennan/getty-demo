0c6ddf4bab4cef44305c59e3cc5bc903494a1bd9
EC2 Default User <ec2-user@ip-172-31-14-135.us-west-2.compute.internal>
2015-02-09 18:14:26 +0000

    Init getty demo

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+
+var app = express();
+app.use('/', express.static(__dirname + '/www'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res) {
+  res.redirect('image-search.html');
+});
+
+app.all('/search-images', function(req, res) {
+  if (!req.body) req.body = {};
+  request.get({
+    url: "https://api.gettyimages.com/v3/search/images",
+    qs: {
+      'phrase': req.body.query,
+      'page_size': 10,
+    },
+    headers: {
+      'Api-Key': "jffpb83bhunc75qk2mjpsgrj",
+    },
+    json: true,
+  }, function(err, response, body) {
+    res.json(body)
+  })
+});
+
+app.listen(process.env.PORT || 3333);
